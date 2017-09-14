d93b59fcd1103a4798f7864d5e72852fbf250f80
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-09-14 22:13:53 +0530

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int num,cube;
+	cout<<"enter number \n";
+	cin>>num;
+	/* cube calculation. */
+	cube=num*num*num;
+	cout<<"cube of a number is"<<cube;
+
+	return 0;
+}
